17648008f4e1bd6c4e40af2b310b953f0d703436
Christopher Hasek <cjh333@gmail.com>
2011-10-11 00:38:05 -0700

    Done with Chapter 4

---
+++ example_user.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/env ruby
+
+class User
+    attr_accessor :name, :email
+
+    def initialize(attributes = {})
+        @name  = attributes[:name]
+        @email = attributes[:email]
+    end
+
+    def formatted_email
+        "#{@name} <#{@email}>"
+    end
+end
